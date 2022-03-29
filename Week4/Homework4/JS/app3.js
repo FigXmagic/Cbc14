d628f96a698121b2b4341639548795da13e1c1db
Fig369 <figueroa.jorge20@gmail.com>
2022-03-29 17:33:59 -0400

    Updated RPS HW

--- Week4/Homework4/JS/app3.js
+++ Week4/Homework4/JS/app3.js
@@ -5,76 +5,74 @@ const game = {
 
 const choices = ["rock", "paper", "scissors"];
 
-var random1 = Math.round(Math.random() * 2);
-var random2 = Math.round(Math.random() * 2);
+function returnResult() {
+  console.clear(); // This is just to prevent console crowding for this example
 
-var bot1 = choices[random1];
-var bot2 = choices[random2];
+  // Move your random number generators inside the function
+  // so you get a new random number each time game is played.
+  const random1 = Math.round(Math.random() * 2);
+  const random2 = Math.round(Math.random() * 2);
 
-console.log("Player 1:" + bot1);
-console.log("Player 2:" + bot2);
+  const bot1 = choices[random1];
+  const bot2 = choices[random2];
 
-let x = bot1;
-let y = bot2;
+  console.log("Player 1:" + bot1);
+  console.log("Player 2:" + bot2);
 
-switch (x) {
-  case "paper":
-    switch (y) {
-      case "rock":
-        console.log("Player 1 Wins!");
-        break;
-      case "scissors":
-        console.log("Player 2 Wins!");
-        break;
-      case "paper":
-        console.log("Draw!");
-        break;
-    }
+  // var result = "";  You don't this if you are keeping score in `game` object
+
+  // No need to call switch on bot1 three times:
+  switch (bot1) {
+    case "paper":
+      switch (bot2) {
+        case "rock":
+          console.log("Player 1 Wins!");
+          game.win++; // Update game win
+          break;
+        case "scissors":
+          console.log("Player 2 Wins!");
+          game.loss++; // Update game loss
+          break;
+        case "paper":
+          console.log("Draw!");
+          break;
+      }
+      break;
+
+    case "rock":
+      switch (bot2) {
+        case "scissors":
+          console.log("Player 1 Wins!");
+          game.win++; // Update game win
+          break;
+        case "paper":
+          console.log("Player 2 Wins!");
+          game.loss++; // Update game loss
+          break;
+        case "rock":
+          console.log("Draw!");
+          break;
+      }
+      break;
+
+    case "scissors":
+      switch (bot2) {
+        case "paper":
+          console.log("Player 1 Wins!");
+          game.win++; // Update game win
+          break;
+        case "rock":
+          console.log("Player 2 Wins!");
+          game.loss++; // Update game loss
+          break;
+        case "scissors":
+          console.log("Draw!");
+          break;
+      }
+      break;
+  }
+
+  // return result;  No need to return result.  Update game object every play instead.
+  console.log(game);
+  return game; // Return game object if needed
 }
-
-switch (x) {
-  case "rock":
-    switch (y) {
-      case "scissors":
-        console.log("Player 1 Wins!");
-        break;
-      case "paper":
-        console.log("Player 2 Wins!");
-        break;
-      case "rock":
-        console.log("Draw!");
-        break;
-    }
-}
-
-switch (x) {
-  case "scissors":
-    switch (y) {
-      case "paper":
-        console.log("Player 1 Wins!");
-        break;
-      case "rock":
-        console.log("Player 2 Wins!");
-        break;
-      case "scissors":
-        console.log("Draw!");
-        break;
-    }
-}
-const x,y = {
-  rock: {
-    name: "Rock",
-    defeats: "scissors",
-  },
-  paper: {
-    name: "Paper",
-    defeats: "rock",
-  },
-  scissors: {
-    name: "Scissors",
-    defeats: "paper",
-  },
-};
-
-if (bot1 != x) console.log(bot2);
-if (bot2 == y) console.log(bot2);
