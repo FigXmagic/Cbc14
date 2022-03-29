2cd4800ff512539d78a0c85cd6e7cb16eab73997
Fig369 <figueroa.jorge20@gmail.com>
2022-03-28 23:13:41 -0400

    UPDATTE WEEK4 FILES AND TODO PROJECT
    added the SVG symbol for the delete button

--- Week4/JavascriptModule.js
+++
@@ -1,9 +0,0 @@
-const para = document.querySelector("p");
-const div = document.getElementById("paragraph");
-
-para.addEventListener("click", updateName);
-
-function updateName() {
-  const name = prompt("Enter a new name");
-  para.textContent = `Player1 ${name}`;
-}
