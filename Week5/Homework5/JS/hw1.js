8eebaab38f95b2916082171e27ef993c7e40ff50
Fig369 <figueroa.jorge20@gmail.com>
2022-03-31 14:44:42 -0400

    Hw1 update

--- Week5/Homework5/JS/hw1.js
+++ Week5/Homework5/JS/hw1.js
@@ -1,4 +1,8 @@
-// define an element
+//Jorge Figueroa//
+//6:40 PM UTC March 31st 2022//
+
+//Start//Food//
+
 const food = [
   "okra",
   "carrots",
@@ -11,19 +15,20 @@ const food = [
   "oranges",
 ];
 console.log(food);
-// remove one element
+
 food.pop();
 console.log(food);
-// add an element
+
 food.push("strawberries");
 console.log(food);
+
 food.shift();
 console.log(food);
-//add element to the front of page
+
 food.unshift("broccoli");
 console.log(food);
 
-// define veggies and fruits
+//Fruits and Veggie//
 
 let veggies = food.slice(0, 5);
 console.log(veggies);
@@ -37,16 +42,24 @@ console.log(food);
 food.splice(2, 2, "brussels sprouts");
 console.log(food);
 
+//Lorem Text//
+
 var text = "Lorem, ipsum dolor sit amet consectetur adipisicing elit.";
 console.log([text[5]]);
-const last = text[text.length - 1];
-console.log([last]);
 
-let lastItem = text.slice(-1);
-console.log(lastItem);
+console.log([text[text.length - 1]]);
+
 var lorem = text.slice(0, 5);
-console.log(lorem);
+
+console.log([lorem]);
+
+//Chores//
+
 var chores1 = "clean, pay bills, groceries";
 var chores2 = "learn JS, doctors office, pick up kids";
+
 var allChores = chores1.split(chores2);
-console.log(allChores);
+
+var x = allChores.concat(chores2);
+
+console.log(x);
