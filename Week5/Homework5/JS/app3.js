5e5a4c3d5f34872509952b775940885026c10e1b
Fig369 <figueroa.jorge20@gmail.com>
2022-04-03 20:29:37 -0400

    Push HW app3

---
+++ Week5/Homework5/JS/app3.js
@@ -0,0 +1,50 @@
+for (i = 1; i < 11; i++) {
+  console.log(i);
+}
+
+while (true) {
+  break;
+}
+
+const bottlesOnTheWall = true;
+let beerBottles = 99;
+
+// Initiate infinite loop
+while (bottlesOnTheWall) {
+  console.log(
+    ` ${beerBottles} bottles of beer on the wall. Take one down pass it around.`
+  );
+  beerBottles--;
+  if (beerBottles === 1) {
+    console.log(
+      " " + "1 bottle of beer on the wall. Take one down pass it around."
+    );
+    break;
+  }
+}
+//original version//
+/* for (var i = 99; i >= 2; i--) {
+  console.log(
+    i + " " + "bottles of beer on the wall. Take one down pass it around."
+  );
+}
+for (var i = 1; i >= 1; i--) {
+  console.log(
+    i + " " + "bottle of beer on the wall. Take one down pass it around."
+  );
+} */
+
+let bools = [true, true, false, true, false, true, true, true];
+for (let i = 0; i <= bools.length; i++) {
+  if (bools[i] === true) {
+    console.log(bools[i]);
+  }
+}
+
+let str = "apples";
+let vowels = ["a", "e", "i", "o", "u"];
+for (let i = 0; i <= str.length; i++) {
+  if (vowels.includes(str[i])) {
+    console.log(str[i]);
+  }
+}
