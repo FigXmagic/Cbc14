c3989b68bad7db0a6a249514069597e63a919300
Fig369 <Figueroa.jorge20@gmail.com>
2022-04-23 23:10:12 -0400

    commit template

--- Week6/XMLHttpRequest/XHR-master/js/xmlhttp.js
+++ Week6/XMLHttpRequest/XHR-master/js/xmlhttp.js
@@ -1,2 +1,23 @@
+let logo = document.querySelector('#action_buttons');
+
+logo.addEventListener('load', (event) => {
+	console.log('Logo has been loaded!');
+});
+
+logo.src = "page1.html";
+/* 
+		function loadDoc() {
+		  var xhttp = new XMLHttpRequest();
+		  xhttp.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
+			  document.getElementById("action_buttons").innerHTML =
+			  this.responseText;
+			}
+		  };
+		  xhttp.open("GET", "ajax_info.txt", true);
+		  xhttp.send();
+		} */
+
 // handle button clicks
 // when the button is clicked, load content into #page_content from the associated /file/ resource
+ 
