eb7c55a626db2f9b5a9a8e70c5013b6fda065ede
Fig369 <Figueroa.jorge20@gmail.com>
2022-04-24 23:45:50 -0400

    Update xmlhttp.js

--- Week6/XMLHttpRequest/XHR-master/js/xmlhttp.js
+++ Week6/XMLHttpRequest/XHR-master/js/xmlhttp.js
@@ -1,7 +1,14 @@
+//Jorge Figueroa//
+//April 24th 2022//
+
+
+//get the onclicks for each page ID//
 document.getElementById("page1").addEventListener("click", loadXMLDoc1); 
 document.getElementById("page2").addEventListener("click", loadXMLDoc2); 
 document.getElementById("page3").addEventListener("click", loadXMLDoc3);
 
+
+//on load get the contents of page1 into inner HTML//
 var xmlhttp = new XMLHttpRequest(),
   method = 'GET',
   url = 'http://127.0.0.1:5500/repo/Cbc14/Week6/XMLHttpRequest/XHR-master/files/page1.html';
@@ -13,6 +20,8 @@ xmlhttp.onload = function () {
 };
 xmlhttp.send();
 
+
+// 3 seperate functions for each onclick handler for each page.html within files//
 function loadXMLDoc1() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
@@ -48,52 +57,6 @@ function loadXMLDoc3() {
   xhttp.open("GET", "http://127.0.0.1:5500/repo/Cbc14/Week6/XMLHttpRequest/XHR-master/files/page3.html", true);
   xhttp.send();
 }
-/* var div = document.querySelector("div");
-const page1 = document.querySelector("#page1");
-const page2 = document.querySelector("#page2");
-const page3 = document.querySelector("#page3");
-
-form.addEventListener("click", function(event) {
-  event.preventDefault();
-  if (event.target == page1) {
-    goButton.classList.add("go");
-    stopButton.classList.remove("stop");
-  } else if (event.target == stopButton) {
-    stopButton.classList.add("stop");
-    goButton.classList.remove("go");
-  }
-});
-
-
-
-function myFunction() {
-	document.getElementById("myCheck").click();
-  }
-
-var form = document.querySelector("form");
-
-form.addEventListener("click", function(event) {
-  event.preventDefault();
-  const classId = event.target.id;
-
-  if (classId == "go") {
-    event.target.className = "go";
-  } else if (classId == "stop") {
-    event.target.className = "stop";
-  }
-});
-
- */
-/* for(let i=1; i<3; i++) {
-        
-	console.log(i); 
- 
-	$("#div" + i).click(
-		function () { alert(i); }
-	);
-}
- */
-
 
 // handle button clicks
 // when the button is clicked, load content into #page_content from the associated /file/ resource
