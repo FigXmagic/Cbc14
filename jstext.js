47ccf8a9de7425d821c13f1720eeadab18dfdcce
Fig369 <Figueroa.jorge20@gmail.com>
2022-04-25 12:56:52 -0400

    Update week 7 & Gitignore
    removed the files in the git ignore and removed any files that were duplicated

--- jstext.js
+++
@@ -1,9 +0,0 @@
-function myDisplayer(something) {
-  document.getElementById("demo").innerHTML = something;
-}
-function myCalculator(num1, num2, num3, myCallback) {
-  let sum = num1 + num2 + num3;
-  myCallback();
-}
-
-myCalculator(5, 5);
