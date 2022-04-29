1b481a2fc91bb15d259c985c965fb1874f766be0
Fig369 <Figueroa.jorge20@gmail.com>
2022-04-29 13:30:47 -0400

    PaperRockScissors hw started

---
+++ Week7/RockPaperScissors/prs.js
@@ -0,0 +1,93 @@
+//1//
+class PaperRockScissors {
+    constructor(name, age) {
+      this.name = name;
+      this.age = age;
+    }
+    paper () {
+      return console.log(this.paper)
+    }
+    rock () {
+        return console.log(this.paper)
+      }
+    scissors () {
+        return console.log(this.paper)
+      }
+  }
+
+  //2//
+  class Player extends PaperRockScissors {
+    constructor(name, age, employeeID) {
+      super(name, age);
+      this.employeeID = employeeID = currentID;
+    }
+    getCompanyName() {
+      return `Web Development Solutions Incorporated`
+    }
+    getEmployeeID() {
+      return this.employeeID
+    }
+  }
+/*
+  //3//
+  class Manager extends Employee{
+    constructor(name, age, employeeID, employees){
+      super(name, age, employeeID,)
+      this.employees = employees
+    }
+    newEmployee(employeeID){
+      return  this.employees.push(employeeID)
+    }
+    getEmployees(){
+      return `${this.employees}`
+    }
+  }
+  
+  
+  //4//
+  class Clerk extends Employee{
+    constructor(name, age, employeeID, supervisorID){
+      super(name, age, employeeID)
+      this.supervisorID = supervisorID
+    }
+    getSupervisor(){
+      return `Supervisor ID: ${this.supervisorID}`
+    }
+  }
+  
+  
+  //5//
+  var currentID = 0;
+  
+  //6//
+  var boss = new Manager("Fig", 33, currentID++, employees=[])
+  
+  
+  //7//
+  var clerk1 = new Clerk("Alice", 44, currentID++, boss.getEmployeeID())
+  
+  
+  //8//
+  var clerk2 = new Clerk("Bob", 64, currentID++, boss.getEmployeeID())
+  
+  
+  
+  //9//
+  boss.newEmployee(` Name: ${this.clerk1.getName()}, EmployeeID: ${this.clerk1.getEmployeeID()} `)
+  
+  /* console.log(boss)
+  console.log(clerk1)
+  console.log(clerk2) */
+  //10//
+/*   boss.newEmployee(` Name: ${this.clerk2.getName()}, EmployeeID: ${this.clerk2.getEmployeeID()} `) */
+  /* console.log(boss)
+  console.log(clerk1)
+  console.log(clerk2.employeeID) */
+  
+  //11//
+/*   console.log(clerk1.getSupervisor())
+  console.log(clerk2.getSupervisor()) */
+  
+  //12//
+/*   console.log(boss.getEmployees())
+  console.log(boss.employees) */ */
