9dee19d728d4a56caaabf290ec8b71c82c04e6c9
Fig369 <Figueroa.jorge20@gmail.com>
2022-05-03 16:02:54 -0400

    update hw

---
+++ Week8/js/hw1.js
@@ -0,0 +1,46 @@
+function promise1() {
+    return new Promise((resolve, reject) => {
+        resolve();
+    })
+};
+
+promise1()
+.then(() => { console.log('success') })
+.catch(() => { console.log('there was an error')})
+
+function promise2(errorBoolean) {
+    return new Promise((resolve, reject) => {
+        if(errorBoolean) {
+            reject('There was an error');
+        } else {
+            resolve('Your data you asked for.');
+        }
+    })
+}
+
+promise2(true)
+  .then(data => {console.log(data)})
+  .catch(err => {console.log(err)})
+
+promise2(false)
+  .then(data => {console.log(data)})
+  .catch(err => {console.log(err)})
+
+const promise3 = new Promise((resolve, reject) => { resolve('promise 3 complete') });
+const promise4 = new Promise((resolve, reject) => { reject('There was an error w/ promise 4')});
+const promise5 = new Promise((resolve, reject) => { resolve('promise 5 complete') });
+console.log(promise5)
+
+proimse3
+.then(data => { console.log(data) })
+.catch(err => { console.log(err) })
+
+Promise.all([promise3, promise4, promise5])
+.then(data => { console.log(data) })
+.catch(err => { console.log(err) })
+
+const promise4 = new Promise((resolve, reject) => { resolve('Promise 4 complete') });
+
+Promise.all([promise3, promise4, promise5])
+.then(data => { console.log(data) })
+.catch(err => { console.log(err) })
