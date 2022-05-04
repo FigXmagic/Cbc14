60a3e95d182df9ad26ad72c7f9feb727c83c8735
Fig369 <Figueroa.jorge20@gmail.com>
2022-05-03 23:03:02 -0400

    Update hw1.js
    Home work 1 completed, Bonus still needs to be completed

--- Week8/js/hw1.js
+++ Week8/js/hw1.js
@@ -1,16 +1,23 @@
+//1//
 function promise1() {
+    //------2-------// //-----3------//
     return new Promise((resolve, reject) => {
-        resolve();
+    //4//
+    resolve();
     })
 };
 
-promise1()
+//--5--//
+promise1()   //--------------6------------------//
 .then(() => { console.log('success') })
 .catch(() => { console.log('there was an error')})
 
+//-----------7--------------//
 function promise2(errorBoolean) {
     return new Promise((resolve, reject) => {
+        //------8------//
         if(errorBoolean) {
+        //------------------9-------------------//
             reject('There was an error');
         } else {
             resolve('Your data you asked for.');
@@ -18,6 +25,7 @@ function promise2(errorBoolean) {
     })
 }
 
+//--------------10----------------//
 promise2(true)
   .then(data => {console.log(data)})
   .catch(err => {console.log(err)})
@@ -26,21 +34,39 @@ promise2(false)
   .then(data => {console.log(data)})
   .catch(err => {console.log(err)})
 
+//---------------------------------------11----------------------------------------//
 const promise3 = new Promise((resolve, reject) => { resolve('promise 3 complete') });
-const promise4 = new Promise((resolve, reject) => { reject('There was an error w/ promise 4')});
-const promise5 = new Promise((resolve, reject) => { resolve('promise 5 complete') });
-console.log(promise5)
 
-proimse3
-.then(data => { console.log(data) })
-.catch(err => { console.log(err) })
+//--------------------------------------------12---------------------------------------------//
+/* const promise4 = new Promise((resolve, reject) => { reject('There was an error w/ promise 4')}); */
 
-Promise.all([promise3, promise4, promise5])
-.then(data => { console.log(data) })
-.catch(err => { console.log(err) })
-
+////---------------------------------------16----------------------------------------////
 const promise4 = new Promise((resolve, reject) => { resolve('Promise 4 complete') });
 
+//---------------------------------------13----------------------------------------//
+const promise5 = new Promise((resolve, reject) => { resolve('promise 5 complete') });
+/* console.log(promise5) */
+
+
+//--------------14----------------//
+/* promise3
+.then(data => { console.log(data) })
+.catch(err => { console.log(err) }) */
+
+//--------------15----------------//
+////--------------17----------------////
 Promise.all([promise3, promise4, promise5])
 .then(data => { console.log(data) })
 .catch(err => { console.log(err) })
+
+//----------18-----------------//
+let xhr = new XMLHttpRequest();
+
+xhr.open('GET', 'http://api.icndb.com/jokes/15', true);
+
+xhr.onload = function () {
+	const joke = JSON.parse(this.responseText).value.joke;
+	console.log(joke);
+};
+
+xhr.send();
