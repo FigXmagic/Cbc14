0ca2ff0ae292fbad916f9ac769927d8cd95748c5
Fig369 <Figueroa.jorge20@gmail.com>
2022-05-05 23:07:16 -0400

    Update export.js

--- Week8/WeekHomework2/export.js
+++ Week8/WeekHomework2/export.js
@@ -2,24 +2,53 @@ function addNums(...nums){
     let res= nums.reduce((num1, num2) => num1 + num2)
     return res
 }
+
+function subNums(...nums){
+    let res= nums.reduce((num1, num2) => num1 - num2)
+    return res
+}
+
+module.exports = {addNum, subNum}
+/* let x = 1
+let y = 2
+let z = 3
+
+console.log(addNums(x,y,z))
+console.log(subNums(x,y,z))
+ */
+
+
+/* function fibonacci(num) {
+    if (num <= 1) return 1;
+  
+    return fibonacci(num - 1) + fibonacci(num - 2);
+}
+
+console.log(fibonacci(6))
 console.log(addNums(1,2))
 console.log(addNums(30,60,90))
 
-let x = 10
-let y = 20
-let z = 30
-let abc = "hello world"
+let x = 1
+let y = 2
+let z = 3
+let abc = ` helloworld `
 let one23 = addNums(x,y,z)
 let three21 = addNums(x * y *z)
+let easy = x * y * z
 
 
 
 console.log(addNums(x,y))
-console.log(addNums([x,y]))
-console.log(addNums([x+y+z * z+x+y]))
-console.log(addNums([x+y+z] * [z+x+y]))
+console.log(addNums(abc,abc))
+console.log(addNums([x,y,z],[z,x,y]))
+console.log(addNums(x,y+z ** z+x,y))
+console.log(addNums([x-y-z ** z-x-y], [x+y+z ** z+x+y]))
 console.log(addNums(x,abc))
 console.log(addNums(abc,y))
 console.log(addNums(one23,three21))
-console.log(addNums(one23 * three21))
-console.log(`${abc.length} (${one23.length} + ${three21.length}`))
+console.log(addNums((easy,x ** z), (one23 ** three21, easy)))
+console.log(three21 * addNums(one23, abc.length))
+console.log(addNums(z+(x * x), y-(y - y), x*(z * z)))
+console.log(easy)
+console.log(easy ** addNums(1 * z,2 * y,3 *x))
+console.log(fibonacci(addNums(x,y**y,z))) */
